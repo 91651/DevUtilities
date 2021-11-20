8f338ae7c41d40d0ea3732fd67285be845c7c507
LZH <mail@521.org.cn>
2021-11-20 22:02:52 +0800

    init project

---
+++ BC.Util/Program.cs
@@ -0,0 +1,31 @@
+using BC.Util.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+// Add services to the container.
+services.AddRazorPages();
+services.AddServerSideBlazor();
+services.AddAntDesign();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
